6b477b413d3a708a9ef130fcd9d381ed0045c759
syang019 <stanleyang13@gmail.com>
2015-02-17 14:04:50 -0800

    Angular added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-animate
+//= require angular-resource
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,13 @@ Bundler.require(*Rails.groups)
 
 module Recordbyte
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+   root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+    config.sass.load_paths << bower_path
+    config.assets.paths << bower_path
+  end
+  # Precompile Bootstrap fonts
+  config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+  # Minimum Sass number precision required by bootstrap-sass
+  ::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max 
   end
 end
