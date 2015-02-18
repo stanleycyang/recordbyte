d2a140d4e0c2ecc529afea501287255648c40d46
syang019 <stanleyang13@gmail.com>
2015-02-18 13:35:41 -0800

    Added bower bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require angular
 //= require angular-animate
 //= require angular-resource
