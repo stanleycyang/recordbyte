7e9c53ef2563c4af2321216ffcb10ef494fc6916
syang019 <stanleyang13@gmail.com>
2015-03-07 20:35:09 -0800

    Fixed serializer error

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,10 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :body, :stars, :book, :user
+  attributes :id, :body, :stars, :poster, :book
 
   has_one :book
+
+  def poster
+    return {id: object.user.id, name: object.user.name}
+  end
 
 end
